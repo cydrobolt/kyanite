71261d2f09b27d9d3605848262c46a02d2dffca3
Cydrobolt <summermontreal@gmail.com>
2014-04-18 19:46:27 -0400

    another db fix, logging

--- kyanite.php
+++ kyanite.php
@@ -8,8 +8,8 @@ $ua = $_SERVER['HTTP_USER_AGENT'];
 $ref = $_SERVER['HTTP_REFERER']||"NOT AVAILABLE";
 $date = date("m/d/Y");
 $now = date("m/d/Y G:i:s");
-$query = "INSERT INTO info (ip,ua,ref,date) VALUES ('$ip','$ua','$ref','$date')";
-$res = $mysqli->query($query) or kyalog("$now - Could not send data to the database.");
+$query = "INSERT INTO stats (ip,ua,ref,date) VALUES ('$ip','$ua','$ref','$date')";
+$res = $mysqli->query($query) or kyalog("$now - Could not send data to the database; error : {$mysqli->error}\n");
 $mysqli->query($query);
 $mysqli->close();
 ?>
